f45a60c7823e76b4818ecd0cbe3c9a504fe8999c
Paula Sierra <Paulacode01@gmail.com>
2021-01-31 21:16:04 -0500

    agregando entorno de desarrollo y servicio de login

---
+++ Users/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+# User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'email'
+        )
+
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'email',
+            'password'
+        )
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'],
+                                        email=validated_data['email'],
+                                        first_name=validated_data['first_name'],
+                                        password=validated_data['password']
+                                        )
+
+        return user
